321223e3cf6b9a5fe8b9187fa302c1b3ca0bc847
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-25 21:38:13 +0100

    Refactor Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const Navbar = () => {
 	return (
-		<header className="flex">
-			<span>LOGO</span>
-			<nav>
-				<ul className="flex">
+		<header className="flex ">
+			<span className="flex-1">LOGO</span>
+			<nav className="flex-1">
+				<ul className="flex justify-around ">
 					<li>HOME</li>
 					<li>FILMY</li>
 					<li>SERIALE</li>
