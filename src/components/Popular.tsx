328123629cbaf1eee45530eadb44a3697a34ba81
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-28 21:25:53 +0100

    Add heading and image to Popular component

--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 
 const Popular = () => {
-	return <div className="p-1 m-3">Popular</div>
+	return (
+		<div
+			className="p-1 m-3 
+	">
+			<h1>Popular</h1>
+			<div className="p-1 m-3">Zdjęcie filmu</div>
+		</div>
+	)
 }
 
 export default Popular
