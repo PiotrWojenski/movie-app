e5540c00fb7b052fdae12c32c252e40fd9bea10e
Piotr Wojenski <pwojenskip@gmail.com>
2024-01-30 16:31:49 +0100

    Refactor Navbar and Newest components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,19 +2,19 @@ import img from '../logo.svg'
 
 const Navbar = () => {
 	return (
-		<header className="flex ">
-			<span className="flex-1 m-2 p-2">
-				<img src={img} alt="logo" />
-			</span>
-			<nav className="flex-1">
-				<ul className="flex justify-around  ">
-					<li>HOME</li>
-					<li>MOVIES</li>
-					<li>SERIES</li>
-					<li>FAVOURTIES</li>
-					<li>PROFILE</li>
-				</ul>
-			</nav>
+		<header className="bg-gray-800 text-white p-4">
+			<div className="container mx-auto flex justify-between items-center">
+				<img src={img} alt="logo" className="h-8" />
+				<nav>
+					<ul className="flex space-x-4">
+						<li className="hover:text-gray-300">HOME</li>
+						<li className="hover:text-gray-300">MOVIES</li>
+						<li className="hover:text-gray-300">SERIES</li>
+						<li className="hover:text-gray-300">FAVOURITES</li>
+						<li className="hover:text-gray-300">PROFILE</li>
+					</ul>
+				</nav>
+			</div>
 		</header>
 	)
 }
--- src/components/Newest.tsx
+++ src/components/Newest.tsx
@@ -13,14 +13,16 @@ const Newest = () => {
 	if (isLoading) return <h2>Loading...</h2>
 
 	return (
-		<div className="p-1 m-3">
-			<h1>ALL</h1>
-			<div className="flex flex-wrap">
+		<div className="p-4 m-3">
+			<h1 className="text-2xl font-bold mb-4">ALL</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{data?.map((item: any) => {
 					return (
-						<div className="p-1 m-1 flex-col">
-							<h3>{item.title}</h3>
-							<img src={item.img} alt={item.title} className="movies-img" />
+						<div className="flex flex-col bg-white rounded-lg overflow-hidden shadow-lg">
+							<img src={item.img} alt={item.title} className=" h-52 object-cover" />
+							<div className="p-4">
+								<h3 className="text-lg font-semibold truncate">{item.title}</h3>
+							</div>
 						</div>
 					)
 				})}
