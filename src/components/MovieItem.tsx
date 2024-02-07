946922210265fb85b39c48122d67c5a17360b11e
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-07 18:49:30 +0100

    Update background color and styling in App.css and MovieItem.tsx

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -8,12 +8,12 @@ type MovieItemProps = {
 
 const MovieItem: React.FC<MovieItemProps> = ({ item }) => {
 	return (
-		<div className="flex flex-col  bg-orange-200 rounded-lg overflow-hidden shadow-lg">
+		<div className="flex flex-col bg-orange-200 rounded-lg overflow-hidden shadow-lg m-4">
 			<Link to={`/movie?title=${item.title}`}>
-				<img src={item.img} alt={item.title} className=" h-auto " />{' '}
+				<img src={item.img} alt={item.title} className="h-56 w-full object-cover" />
 			</Link>
-			<div className="p-3">
-				<h3 className="text-lg font-bold ">{item.title}</h3>
+			<div className="p-4">
+				<h3 className="text-lg font-bold text-center">{item.title}</h3>
 			</div>
 		</div>
 	)
