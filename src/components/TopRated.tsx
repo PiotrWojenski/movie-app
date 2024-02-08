c182177d58ac8169a64bc1345a40d8a1cb671476
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-08 21:43:58 +0100

    Add top rated movies section to TopRated component

--- src/components/TopRated.tsx
+++ src/components/TopRated.tsx
@@ -1,5 +1,26 @@
+import React from 'react'
+import useFetch from '../hooks/useFetch'
+import MovieItem from './MovieItem'
+
 const TopRated = () => {
-	return <div className="p-1 m-3">Top Rated</div>
+	const { data: movies, isLoading, isError } = useFetch()
+
+	if (isLoading) return <div>Loading...</div>
+	if (isError) return <div>Error</div>
+
+	// Sortowanie filmów według oceny i wzięcie pierwszych 10
+	const topRatedMovies = movies?.sort((a, b) => b.rating - a.rating).slice(0, 10)
+
+	return (
+		<div className="p-4 m-4">
+			<h1 className="text-2xl font-bold mb-4">Najlepiej Oceniane</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				{topRatedMovies?.map(movie => (
+					<MovieItem key={movie.id} item={movie} />
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default TopRated
