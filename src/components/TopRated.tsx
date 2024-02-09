75533a4dbafffbdc802ed8bacc8eb18c7e95174d
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-09 22:41:06 +0100

    Update TopRated component heading

--- src/components/TopRated.tsx
+++ src/components/TopRated.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import useFetch from '../hooks/useFetch'
 import MovieItem from './MovieItem'
 
@@ -12,7 +11,7 @@ const TopRated = () => {
 
 	return (
 		<div className="p-4 m-4">
-			<h1 className="text-2xl font-bold mb-4">Najlepiej Oceniane</h1>
+			<h1 className="text-2xl font-bold mb-4">Top Rated</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{topRatedMovies?.map(movie => (
 					<MovieItem key={movie.id} item={movie} />
