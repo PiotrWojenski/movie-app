046d3231a1f7a16a8777aafc5e8d261750be5b68
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-10 17:26:00 +0100

    Update background color and navbar styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,11 +3,12 @@ import { Link } from 'react-router-dom'
 
 const Navbar = () => {
 	return (
-		<header className="bg-gray-800 text-white p-4">
-			<div className="container mx-auto flex justify-between items-center">
-				<img src={img} alt="logo" className="h-8" />
+		<header className="bg-gray-800 text-white p-5 text-3xl  font-bold">
+			<div className="container mx-auto flex justify-between items-center h-9">
+				<img src={img} alt="logo" className="flex-1 items-start h-6 text-pink-700" />
+				<div className="flex-1">Search</div>
 				<nav>
-					<ul className="flex space-x-4">
+					<ul className="flex-1 ">
 						<Link to="/">Home</Link>
 						<Link to="/movies">Movies</Link>
 						<Link to="/series">Series</Link>
