a97878f3196f9ae15e4cf9e28df8197db74462d2
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-10 17:52:35 +0100

    Update Navbar component styling

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
 	return (
 		<header className="bg-gray-800  text-white p-2 text-3xl  font-bold">
 			<div className="flex justify-between items-center h-9">
-				<img src={img} alt="logo" className="flex-1 items-start  h-6 text-pink-700" />
-				<div className="flex-1">Search</div>
+				<img src={img} alt="logo" className="flex-1 items-start  h-6 " />
+				<div className="flex-1 text-center">Search</div>
 				<nav>
-					<ul className="flex flex-1 justify-between items-center ">
+					<ul className="flex flex-2 justify-between items-center ">
 						<Link to="/">Home</Link>
 						<Link to="/movies">Movies</Link>
 						<Link to="/series">Series</Link>
