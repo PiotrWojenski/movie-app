98c0288e9ca8f1d10888d044ff4579f1d7cf8699
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-10 17:56:31 +0100

    Add logo-img class to App.css and Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 	return (
 		<header className="bg-gray-800  text-white p-2 text-3xl  font-bold">
 			<div className="flex justify-between items-center h-9">
-				<img src={img} alt="logo" className="flex-1 items-start  h-6 " />
+				<img src={img} alt="logo" className="logo-img flex-1 items-start  h-6 " />
 				<div className="flex-1 text-center">Search</div>
 				<nav>
 					<ul className="flex flex-2 justify-between items-center ">
