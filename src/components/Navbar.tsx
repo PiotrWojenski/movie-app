b4fa937822ba2825b4630869195ea8430cf2a149
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-12 14:53:06 +0100

    Update styles and add logo to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom'
+import Box from '@mui/material/Box'
+import TextField from '@mui/material/TextField'
 
 const Navbar = () => {
 	return (
 		<header className="bg-gray-800 text-white p-4 text-xl font-bold">
 			<div className="container mx-auto flex justify-between items-center">
 				<div className="flex items-center">
-					<span>ratingAppski</span>
+					<span className="logo-img" style={{ fontFamily: "'Protest Riot', sans-serif" }}>
+						ratingAppski
+					</span>
 				</div>
 				<div className="hidden md:block">Search</div>
 				<nav>
