e37b8e341268344c265e31c8cf39e05ba4ee5cda
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-12 15:16:03 +0100

    Refactor Movies page to display filtered movies

--- src/pages/Movies.tsx
+++ src/pages/Movies.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import MovieItem from '../components/MovieItem'
+import useFetchMovies from '../hooks/useFetchMovies'
 
 const Movies = () => {
-	return <div>Movies</div>
+	const { movies } = useFetchMovies()
+	const showMovies = movies?.filter((item: any) => item.type === 'movie')
+
+	return (
+		<div className="p-4 m-4">
+			<h1 className="text-2xl font-bold mb-4">Movies</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				{showMovies?.map((item: any) => {
+					return <MovieItem key={item.id} item={item} />
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default Movies
