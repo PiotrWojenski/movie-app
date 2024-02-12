addfb7e2e588f235b69fdbf6f34e041a0fbb26e7
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-12 15:18:22 +0100

    Refactor Series page to display series items using MovieItem component

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import MovieItem from '../components/MovieItem'
+import useFetchMovies from '../hooks/useFetchSeries'
 
 const Series = () => {
-	return <div>Series</div>
+	const { series } = useFetchMovies()
+	const showSeries = series?.filter((item: any) => item.type === 'series')
+
+	return (
+		<div className="p-4 m-4">
+			<h1 className="text-2xl font-bold mb-4">Series</h1>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+				{showSeries?.map((item: any) => {
+					return <MovieItem key={item.id} item={item} />
+				})}
+			</div>
+		</div>
+	)
 }
 
 export default Series
