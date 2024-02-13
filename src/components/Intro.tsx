409b39ca95703d2fc68ec7431e6f9d9002879320
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-13 08:54:52 +0100

    Refactor Intro and Navbar components

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,7 +1,5 @@
-import React from 'react'
-
 const Intro = () => {
-	return <div className="mt-8 p-1 m-3">Intro video </div>
+	return <div className=" p-1 m-3">Intro video </div>
 }
 
 export default Intro
