80bf0b6fbeafd66057edde981e160d9c0cf14b0f
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-15 16:36:37 +0100

    Refactor TopRated component to sort movies by rating and display movie title as key

--- src/components/TopRated.tsx
+++ src/components/TopRated.tsx
@@ -7,14 +7,16 @@ const TopRated = () => {
 	if (isLoading) return <div>Loading...</div>
 	if (isError) return <div>Error</div>
 
-	const topRatedMovies = (movies as { id: number; rating: number }[])?.sort((a, b) => b.rating - a.rating).slice(0, 10)
+	const topRatedMovies = (movies as { title: string; rating: number }[])
+		?.sort((a, b) => b.rating - a.rating)
+		.slice(0, 10)
 
 	return (
 		<div className="p-4 m-4">
 			<h1 className="text-2xl font-bold mb-4">Top Rated</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{topRatedMovies?.map(movie => (
-					<MovieItem key={movie.id} item={movie} />
+					<MovieItem key={movie.title} item={movie} />
 				))}
 			</div>
 		</div>
