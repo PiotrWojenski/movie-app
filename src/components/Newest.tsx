f9042c449537f9124c7a5ea80d5a860e8d676b9c
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-15 16:35:22 +0100

    Update key prop in MovieItem component

--- src/components/Newest.tsx
+++ src/components/Newest.tsx
@@ -12,7 +12,7 @@ const Newest = () => {
 			<h1 className="text-2xl font-bold mb-4">Newest</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{newMovies?.map((item: any) => {
-					return <MovieItem key={item.id} item={item} />
+					return <MovieItem key={item.title} item={item} />
 				})}
 			</div>
 		</div>
--- src/components/Popular.tsx
+++ src/components/Popular.tsx
@@ -9,7 +9,7 @@ const Popular = () => {
 			<h1 className="text-2xl font-bold mb-4">POPULAR</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{moviesPopular?.map((item: any) => {
-					return <MovieItem key={item.id} item={item} />
+					return <MovieItem key={item.title} item={item} />
 				})}
 			</div>
 		</div>
