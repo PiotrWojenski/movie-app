10f9eedae3873005336f5781acb89d8dd77d5904
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-22 21:12:22 +0100

    Refactor Intro component to remove unused code

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import { Carousel } from 'react-bootstrap'
-import video1 from '../assets/got-zwiastun.mp4' // Ensure the path to the file is correct
 
 const Intro = () => {
-	return (
-		
-	)
+	return <div>Intro video</div>
 }
 
 export default Intro
