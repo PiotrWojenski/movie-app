00800a2eb1884dce92855095334292223df25a54
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-22 21:11:53 +0100

    Remove unused code from Intro component

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -4,32 +4,7 @@ import video1 from '../assets/got-zwiastun.mp4' // Ensure the path to the file i
 
 const Intro = () => {
 	return (
-		<Carousel>
-			<Carousel.Item>
-				<video className="d-block w-100" autoPlay playsInline loop muted>
-					<source src={video1} type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-				<Carousel.Caption>
-					<h3>Game of Thrones</h3>
-					<p>
-						Nine noble families fight for control over the lands of Westeros, while an ancient enemy returns after being
-						dormant for millennia.
-					</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			<Carousel.Item>
-				<video className="d-block w-100" autoPlay playsInline loop muted>
-					<source src="https://mdbcdn.b-cdn.net/img/video/forest.mp4" type="video/mp4" />
-					Your browser does not support the video tag.
-				</video>
-				<Carousel.Caption>
-					<h3>Second Slide</h3>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-				</Carousel.Caption>
-			</Carousel.Item>
-			{/* You can add more Carousel.Item elements as needed */}
-		</Carousel>
+		
 	)
 }
 
