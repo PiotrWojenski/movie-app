d6d25a72a197c6fc3eea5c3dbe394bd3c4fd72ce
Piotr Wojenski <pwojenskip@gmail.com>
2024-02-28 21:58:58 +0100

    Update styles and layout in App.css and Series.tsx

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -6,7 +6,7 @@ const Series = () => {
 	const showSeries = series?.filter((item: any) => item.type === 'series')
 
 	return (
-		<div className="p-4 m-4">
+		<div className="p-5 m-4">
 			<h1 className="text-2xl font-bold mb-4">Series</h1>
 			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 				{showSeries?.map((item: any) => {
