6c27929c4ed220c1aebb0892855fbb19b2fe159c
Piotr Wojenski <pwojenskip@gmail.com>
2024-03-18 18:18:58 +0100

    Fix bug in login functionality

---
+++ src/components/CustomArrow.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CustomArrow: React.FC<{
+	className?: string
+	style?: React.CSSProperties
+	onClick?: () => void
+	isNext?: boolean
+}> = ({ className, style, onClick, isNext }) => {
+	const baseStyle = 'z-10 absolute top-1/2 transform -translate-y-1/2 cursor-pointer'
+	return (
+		<div
+			className={`${className} ${isNext ? 'right-0 mr-2' : 'left-0 ml-2'} ${baseStyle}`}
+			style={{ ...style, display: 'block', background: 'grey' }}
+			onClick={onClick}
+		/>
+	)
+}
+
+export default CustomArrow
