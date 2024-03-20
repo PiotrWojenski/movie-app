444d9045678ca435c5b48cc1b8112a781de11e57
Piotr Wojenski <pwojenskip@gmail.com>
2024-03-20 21:35:13 +0100

    Refactor Intro component to use react-slick for carousel

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,39 +1,48 @@
-import { Carousel } from 'react-bootstrap'
+import React from 'react'
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
 import video1 from '../assets/got.mp4'
 import video2 from '../assets/lord of ring.mp4'
 import video3 from '../assets/Shawshank.mp4'
 
 const Intro = () => {
+	// Settings for the slider
+	const settings = {
+		dots: true, // Enable dot indicators
+		infinite: true, // Infinite looping
+		speed: 500, // Animation speed
+		slidesToShow: 1, // Show one slide at a time
+		slidesToScroll: 1, // Scroll one slide at a time
+		autoplay: true, // Enable autoplay
+		autoplaySpeed: 2000, // Autoplay speed in milliseconds
+		pauseOnHover: true, // Pause autoplay on hover
+	}
+
 	return (
 		<div className="flex justify-center mt-4">
-			<Carousel className="w-1/2 h-1/3" interval={null}>
-				<Carousel.Item className="w-1/2 h-1/3">
+			<Slider {...settings} className="w-1/2 h-1/3">
+				<div>
 					<video className="d-block w-full h-full" playsInline loop muted autoPlay>
 						<source src={video1} type="video/mp4" />
 					</video>
-					<Carousel.Caption className="text-white text-3xl">
-						<p>Game of Thrones</p>
-					</Carousel.Caption>
-				</Carousel.Item>
+					<div className="text-white text-3xl">Game of Thrones</div>
+				</div>
 
-				<Carousel.Item className="w-1/2 h-1/3">
+				<div>
 					<video className="d-block w-full h-full" playsInline loop muted autoPlay>
 						<source src={video2} type="video/mp4" />
 					</video>
-					<Carousel.Caption className="text-white text-3xl">
-						<p>Lord of Ring</p>
-					</Carousel.Caption>
-				</Carousel.Item>
+					<div className="text-white text-3xl">Lord of Ring</div>
+				</div>
 
-				<Carousel.Item className="w-1/2 h-1/3">
+				<div>
 					<video className="d-block w-full h-full" playsInline loop muted autoPlay>
 						<source src={video3} type="video/mp4" />
 					</video>
-					<Carousel.Caption className="text-white text-2xl">
-						<p>Shawshank</p>
-					</Carousel.Caption>
-				</Carousel.Item>
-			</Carousel>
+					<div className="text-white text-2xl">Shawshank</div>
+				</div>
+			</Slider>
 		</div>
 	)
 }
