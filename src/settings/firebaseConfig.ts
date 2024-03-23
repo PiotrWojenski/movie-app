408a91686599c16513a26be223129364882017a9
Piotr Wojenski <pwojenskip@gmail.com>
2024-03-23 21:42:51 +0100

    Add firebase configuration file

---
+++ src/settings/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// firebase-config.ts
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+
+const firebaseConfig = {
+	apiKey: 'API_KEY',
+	authDomain: 'AUTH_DOMAIN',
+	projectId: 'PROJECT_ID',
+	storageBucket: 'STORAGE_BUCKET',
+	messagingSenderId: 'MESSAGING_SENDER_ID',
+	appId: 'APP_ID',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
