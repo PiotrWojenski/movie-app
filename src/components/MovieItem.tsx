b071d54ab955b479cf221f06a36788a9f7084ef4
Piotr Wojenski <pwojenskip@gmail.com>
2024-03-25 21:25:42 +0100

    Refactor MovieItem component to improve UI and add favorite functionality

--- src/components/MovieItem.tsx
+++ src/components/MovieItem.tsx
@@ -1,9 +1,9 @@
-import { movieType } from '../types/myTypes'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { movieType } from '../types/myTypes'
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder'
 import FavoriteIcon from '@mui/icons-material/Favorite'
 import favouriteContext from '../context/favouriteContext'
-import { useContext } from 'react'
 
 type MovieItemProps = {
 	item: movieType
@@ -23,17 +23,17 @@ const MovieItem: React.FC<MovieItemProps> = ({ item }) => {
 	}
 
 	return (
-		<div className="movie-item flex flex-col bg-orange-200 rounded-lg overflow-hidden shadow-lg m-4">
+		<div className="relative movie-item m-4 overflow-hidden rounded-lg shadow-lg">
 			<Link to={`/movie?title=${encodeURIComponent(item.title || '')}`}>
-				<img src={item.img} alt={item.title} className="h-56 w-full object-cover" />
+				<img src={item.img} alt={item.title} className="h-full w-full object-cover rounded-lg" />
 			</Link>
-			<div className="p-4">
-				<h3 className="text-lg font-bold text-center">{item.title}</h3>
-				<div className="text-center">
+			<div className="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent">
+				<h3 className="text-3xl font-bold text-white shadow-2xl shadow-black">{item.title}</h3>
+				<div className="absolute top-0 right-0 p-2">
 					{isFavourite ? (
 						<FavoriteIcon className="text-red-500 cursor-pointer" onClick={toggleFavourite} />
 					) : (
-						<FavoriteBorderIcon className="hover:text-red-500 cursor-pointer" onClick={toggleFavourite} />
+						<FavoriteBorderIcon className="text-white hover:text-red-500 cursor-pointer" onClick={toggleFavourite} />
 					)}
 				</div>
 			</div>
