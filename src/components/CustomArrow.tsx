9f3581313285048655ae64bc841b21eb28725296
Piotr Wojenski <pwojenskip@gmail.com>
2024-03-25 21:17:24 +0100

    Add height and width to CustomArrow component

--- src/components/CustomArrow.tsx
+++ src/components/CustomArrow.tsx
@@ -6,7 +6,7 @@ const CustomArrow: React.FC<{
 	onClick?: () => void
 	isNext?: boolean
 }> = ({ className, style, onClick, isNext }) => {
-	const baseStyle = 'z-10 absolute top-1/2 transform -translate-y-1/2 cursor-pointer'
+	const baseStyle = 'z-10 absolute top-1/2 transform -translate-y-1/2 cursor-pointer h-10 w-10'
 	return (
 		<div
 			className={`${className} ${isNext ? 'right-0 mr-2' : 'left-0 ml-2'} ${baseStyle}`}
